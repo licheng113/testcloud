babce56b856c38cf1366377e3d180ba68bb99478
licheng <1137333584@qq.com>
2021-04-25 15:28:00 +0800

    test

--- boot-api/src/main/java/com/wxf/controller/TestController.java
+++ boot-api/src/main/java/com/wxf/controller/TestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api")
 public class TestController {
-    //git
+
     @Autowired
     Testclient testclient;
     @RequestMapping("/test")
